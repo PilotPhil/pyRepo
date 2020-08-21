faa7f605196350ab3ef9aae582244f929f17421f
phil.pilot <632913013@qq.com>
2020-08-21 21:30:02 +0800

    add

--- 9.串口助手pyqt/SerialHelper.py
+++ 9.串口助手pyqt/SerialHelper.py
@@ -1,24 +1,62 @@
 import sys
 import numpy as np
-from PyQt5.QtWidgets import QApplication,QWidget,QPushButton
-from PyQt5.QtCore import QCoreApplication,pyqtSignal,QObject
-from PyQt5.uic import loadUi
+from matplotlib import pyplot as plt
 from UI_SerialPanel import Ui_window
+from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtSerialPort import QSerialPort,QSerialPortInfo
 
 # 串口类
 class SerialHelper(QWidget): # 传入UI文件Ui_window
-
-    ms=sig()
 
     def __init__(self,parent=None):
         super(QWidget,self).__init__(parent) # 用super非显式的调用父类构造函数
 
         self.ui = Ui_window()
         self.ui.setupUi(self) # 设置UI
-        self.setWindowTitle("串口助手---Pilot.Phil") # 设置窗口标题
+        self.setWindowTitle("串口助手-Pilot.Phil") # 设置窗口标题
 
-    def do(self):
-        print("mysignal did")
+        self.findPorts()
+
+        self.openPort()
+
+
+
+    def findPorts(self):
+        com_list = QSerialPortInfo.availablePorts()
+        for com in com_list:
+            self.ui.cb_serialPortName.addItem(str(com.portName())) #
+            print(com.portName()) # 可用端口名
+            print(com.description()) # 硬件描述
+            # print(com.productIdentifier()) # 返回设备编号
+            # print(com.standardBaudRates()) # 返回设备的支持波特率列表
+
+
+    def openPort(self):
+        # 1.设置端口号
+        myCom=QSerialPort()
+        com=self.ui.cb_serialPortName.currentText()
+        myCom.setPortName(com)
+
+        # 2.设置波特率
+        bradu=self.ui.cb_bradrate.currentText()
+        myCom.setBaudRate(int(bradu))
+
+        # 3.设置数据位
+        databit=self.ui.cb_dataBits.currentText()
+        myCom.setDataBits(int(databit))
+
+        # 4.设置校验位
+        # parity=self.ui.cb_parity.currentText()
+        # myCom.setParity(parity)
+
+        # 5.设置停止位
+        stopbit=self.ui.cb_stopBits.currentText()
+        myCom.setStopBits()
+
+
+
+
+
 
 
 
